0d57757c4d6269f6b10a5f2f9b277464101ad74d
Gwenael-B <gwenael.beltzung@gmail.com>
2024-12-09 11:29:17 +0100

    Version finale et ajout des commentaires pour la fonction enigme_pere_fouras et création de la fonction introduction dans fonction_utiles.py.

--- enigme_pere_fouras.py
+++ enigme_pere_fouras.py
@@ -1,28 +1,32 @@
 import json
 import random
 
-
+#Cette fonction permet de charger toutes les données du fichier enigmesPF.json dans la variable 'donnes' et la retourne.
 def charger_enigmes(dossier):
     with open(dossier + "/enigmesPF.json", "r", encoding='utf8') as enigmesPF:
         donnees = json.load(enigmesPF)
         return donnees
 
+#Cette fonction permet de choisir au hasard une énigme, converti la réponse en minuscule puis affiche la question.
+#La réponse du candidat est ensuite converti en minuscule et comparée à la réponse attendue, si la réponse est la même,
+# la fonction retourne True et un message de victoire est affiché sinon le nombre d'essais est décrémenté de un.
+#Si au bout de trois essais le candidat n'a pas trouvé la réponse, un message affiche la défaite et la bonne réponse puis retourne False.
 def enigme_pere_fouras():
     tentatives_restantes = 3
-    reponse_final_machine = ""
     liste_enigmes = charger_enigmes("./data")
     enigme = random.choice(liste_enigmes)
     reponse_machine = enigme["reponse"]
-    print("Pour gagner la clé, répondez correctement à cette énigme !")
-    print(enigme["question"])
+    reponse_final_machine = ""
     for i in range(len(reponse_machine)):
         if 65 <= ord(reponse_machine[i]) <= 91:
             reponse_final_machine = reponse_final_machine + chr(ord(reponse_machine[i]) + 32)
         else:
             reponse_final_machine = reponse_final_machine + reponse_machine[i]
+    print("Pour gagner la clé, répondez correctement à cette énigme !")
+    print(enigme["question"])
     while tentatives_restantes > 0:
         reponse_final_candidat = ""
-        reponse_candidat = input("Saisissez votre réponse : ")
+        reponse_candidat = input("Saisissez votre réponse avec le determinant : ")
         for i in range(len(reponse_candidat)):
             if 65 <= ord(reponse_candidat[i]) <= 91:
                 reponse_final_candidat = reponse_final_candidat + chr(ord(reponse_candidat[i])+32)
@@ -37,7 +41,6 @@ def enigme_pere_fouras():
                 print("Votre réponse est incorrecte, il vous reste", tentatives_restantes, "tentatives.")
             else:
                 print("Votre réponse est incorrecte, il vous reste", tentatives_restantes, "tentative.")
-
     print("Dommage, vous avez perdu, la réponse était :", reponse_final_machine)
     return False
 
---
+++ fonctions_utiles.py
@@ -0,0 +1,3 @@
+def introduction():
+    print("Bonjour et bien venu dans FORT BOYARD SIMULATOR.\nVous allez faire face à 4 défis. Pour chaque défis remporté, vous gagnerez une clé.\nIl vous faut obtenir au minimum 3 clé pour espérer accéder à la salle du trésor.")
+    print("Vous aurez une épreuve de mathématiques, une épreuve de hasard, une épreuve de logique et enfin l'énigme de père Fouras")
