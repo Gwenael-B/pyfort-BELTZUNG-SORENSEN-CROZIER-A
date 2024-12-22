f5a10587dadaecc2eb12545dd0ef012049e61c16
Gwenael-B <gwenael.beltzung@gmail.com>
2024-12-22 15:10:00 +0100

    Changement erreurs.

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -18,23 +18,24 @@ def resoudre_equation_lineaire():
     a = random.randint(1,10)
     b = random.randint(1,10)
     x = (-b)/a
+    #Pour qu'il n'y ait que 2 chiffres après la virgule.
     x = x * 100
     x = (int(x))/100
     return a, b, x
 
 #Cette fonction appelle la fonction resoudre_equation_lineaire() pour obtenir a, b et x,
 # elle s'assure ensuite que la saisie de l'utilisateur est correcte puis compare ce résultat à celui
-# trouvé par l'algorithme, si le résultat est le même, la fonction retourne True sinon False.
+# trouvé par l'algorithme. Si le résultat est le même, la fonction retourne True sinon False.
 def epreuve_math_equation():
     a, b, x = resoudre_equation_lineaire()
     print("Résoudre l'équation ", a, "x +", b, "= 0.\n")
     while True:
         try:
-            reponse = float(input("Quelle est la valeur de x à deux chiffres après la virgule sans arrondis: "))
+            reponse = float(input("Quelle est la valeur de x à deux chiffres après la virgule sans arrondi: "))
             break
         except ValueError:
             print("Votre réponse n'est pas valide.\n")
-    if reponse == x: #Comment faire si réponse avec plus de x décimales ?
+    if reponse == x:
         print("Correct! Vous gagnez une clé.")
         return True
     else:
@@ -42,6 +43,7 @@ def epreuve_math_equation():
         return False
 
 #Deuxième épreuve de math : détermination du nombre premier (supérieur ou égale) le plus proche d'un entier n.
+
 #Cette fonction permet de définir si un entier n passé en paramètre est un nombre premier.
 def est_premier(n):
     multiple = 0
@@ -71,11 +73,11 @@ def premier_plus_proche(n):
 
 #Cette fonction permet de générer un entier n compris entre 10 et 20 inclus grâce à la fonction random.randint(),
 # elle s'assure ensuite que la saisie de l'utilisateur est correcte puis compare ce résultat à celui
-# trouvé par l'algorithme, si le résultat est le même, la fonction retourne True sinon False.
+# trouvé par l'algorithme. Si le résultat est le même, la fonction retourne True sinon False.
 def epreuve_math_premier():
     n = random.randint(10,20)
     reponse_machine = premier_plus_proche(n)
-    print("Trouver le nombre premier supérieur ou égale le plus proche de ", n) #Faut-il mettre supérieur ou égale ?
+    print("Trouver le nombre premier supérieur ou égale le plus proche de ", n)
     while True:
         try:
             reponse_candidat = int(input("Votre réponse : "))
@@ -90,6 +92,7 @@ def epreuve_math_premier():
         return False
 
 #Troisième épreuve de math : roulette mathematique, calculs entre 5 entiers (addition, soustraction ou multiplication).
+
 #Cette fonction permet de créer une liste de 5 entiers compris entre 1 et 20 inclus grâce à la fonction random.randint(),
 # elle permet ensuite de choisir aléatoirement entre l'addition, la soustraction ou la multiplication grâce à la
 # fonction random.choice(), elle calcule ensuite les opérations demandées puis elle s'assure que la saisie
@@ -100,17 +103,15 @@ def epreuve_roulette_mathematique():
     for i in range(5):
         Liste_nb_roulette.append(random.randint(1,20))
     operation = random.choice(["addition", "soustraction", "multiplication"])
-    if operation == "addition":
-        reponse_machine = Liste_nb_roulette[0]
-        for i in range(1,5):
+    #Initialisation variable reponse_machine.
+    reponse_machine = Liste_nb_roulette[0]
+    #Calcule réponse machine en fonction de l'opération.
+    for i in range(1,5):
+        if operation == "addition":
             reponse_machine = reponse_machine + Liste_nb_roulette[i]
-    elif operation == "soustraction":
-        reponse_machine = Liste_nb_roulette[0]
-        for i in range(1,5):
+        elif operation == "soustraction":
             reponse_machine = reponse_machine - Liste_nb_roulette[i]
-    else:
-        reponse_machine = Liste_nb_roulette[0]
-        for i in range(1, 5):
+        else:
             reponse_machine = reponse_machine * Liste_nb_roulette[i]
     print("Nombres sur la roulette : ", Liste_nb_roulette[:], "\nCalculer le résultat en combinant ces nombres avec une ", operation)
     while True:
