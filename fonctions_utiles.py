5e89ba7dcd6bf2d4e43bfc64df58be747d2713d9
clarence <clarence.crozier@proton.me>
2024-12-26 11:40:16 +0100

    Ajout de la fonction pour enregistrer l'historique aux fonctions utiles

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,9 +1,9 @@
 def introduction():
     print("Bonjour et bienvenue dans FORT BOYARD SIMULATOR.\nVous allez faire face à 4 défis. Pour chaque défi remporté, vous gagnerez une clé.\nIl vous faut obtenir au minimum 3 clés pour espérer accéder à la salle du trésor.")
-    print("Vous aurez une épreuve de mathématiques, une épreuve de hasard, une épreuve de logique et enfin l'énigme de père Fouras\n")
+    print("Vous aurez une épreuve de mathématiques, une épreuve de hasard, une épreuve de logique et enfin l'énigme de père Fouras.\n")
 
 def menu_epreuves():
-    print("Choisissez une épreuve parmi les quatre suivantes.\n")
+    print("Choisissez une épreuve parmi les quatre suivantes : \n")
     print("1. Epreuve de Mathématiques\n2. Epreuve de Logique\n3. Epreuve du hasard\n4. Enigme du Père Fourras")
     choix = input("Choix : ")
     while choix not in ["1","2","3","4"]:  #Vérifie que le choix saisi correspond à une épreuve
@@ -12,7 +12,7 @@ def menu_epreuves():
 
 def composer_equipe():
     nb_joueurs = input("Combien de joueurs souhaitez-vous inscrire dans l'équipe (nombre maximum de trois joueurs) : ")
-    while nb_joueurs not in ["1","2","3"]:  #Vérifie que le nombre de joueurs est bien entre 0 et 3
+    while nb_joueurs not in ["1","2","3"]:  #Vérifie que le nombre de joueurs est bien entre 1 et 3
         nb_joueurs = input("Choix invalide. Veuillez réessayez : ")
     verif_leader = False  #Création de la variable pour vérifier si l'un des joueurs est déjà leader
     equipe = []  #Initialisation de la liste équipe qui contiendra les infos des joueurs
@@ -48,3 +48,28 @@ def choisir_joueur(equipe):
     while choix not in taille:  #Tant que le choix du numéro du joueur n'est pas dans la liste cela signifie que le choix n'est pas valide, on redemande alors un numéro
         choix = input("Choix incorrect, veuillez réessayez : ")
     return equipe[int(choix)-1]
+
+def enregistrer_historique(nom, nb_cle, nb_victoire):
+    with open("historique.txt", "r") as f :  #Ouvre le fichier historique en lecture seul
+        dico_principal = {}  #Initialise le dictionnaire qui stockera toutes les informations des joueurs
+        i = 0
+        f.readline()  #Saute les deux premières lignes car elles ne contiennent pas d'informations, elles servent juste à rendre les valeurs plus visibles
+        f.readline()
+        for ligne in f:  #On parcourt chaque ligne du fichier historique en partant de la troisième
+            ligne = ligne.split()  #On utilise la fonction split pour séparer les mots
+            dico_joueur = {"nom": ligne[0], "nb_cle": int(ligne[2]), "nb_victoire": int(ligne[4])}  #Initialisation du dictionnaire qui contient toutes les données d'un joueur ligne par ligne
+            dico_principal[i] = dico_joueur  #On ajoute ce dictionnaire au dictionnaire principal qui contiendra toutes les informations
+            i = i + 1
+        for i in range(len(dico_principal)):  #On parcourt le dictionnaire principal
+            if dico_principal[i]["nom"] == nom:  #On vérifie si le joueur donné comme argument de la fonction est déjà dans le dictionnaire principal
+                dico_principal[i]["nb_cle"] = int(dico_principal[i]["nb_cle"]) + nb_cle  #On ajoute le nombre de clés qu'il a obtenus au nombre de clés déjà présents
+                dico_principal[i]["nb_victoire"] = int(dico_principal[i]["nb_victoire"]) + nb_victoire  #Pareil pour le nombre de victoires
+                break  #On sort de la boucle for pour ne pas parcourir tous les dictionnaires des joueurs une fois qu'on a trouvé le bon
+        else:  #Si le nom du joueur mis comme argument dans la fonction n'est pas encore dans le dictionnaire principal, on le rajoute
+            dico_principal[len(dico_principal)] = {"nom": nom, "nb_cle": nb_cle, "nb_victoire": nb_victoire}
+    with open("historique.txt", "w") as f:  #On ouvre maintenant le fichier historique en écriture pour modifier les informations des joueurs
+        f.write(f"{'Nom':^10} {"|"} {'Cles':^5} {"|"} {'Victoires':^5}\n")  #On réécrit la mise en page comme le fichier est compressé en ouvrture, les :^nombre servent à aligner les mots
+        f.write("-------------------------------\n")
+        for i in range(len(dico_principal)):  #On parcourt notre dictionnaire principal
+            ligne = f"{dico_principal[i]['nom']:^10} {"|"} {dico_principal[i]['nb_cle']:^5} {"|"} {dico_principal[i]['nb_victoire']:^5}\n"  #On prépare le texte à écrire dans la ligne comme f.write() ne prend qu'un argument
+            f.write(ligne)  #On écrit les informations d'un joueur à chaque ligne
